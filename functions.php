36e9d263112f101585c18fa535263b5fb14d094c
bridos <giavridis_constantine@hotmail.com>
2016-11-18 12:51:53 +0200

    added comments to the script

--- functions.php
+++ functions.php
@@ -1,58 +1,51 @@
 <?php
 include 'setarray.php';
-$cardarray = array();
-$curlarray=array();
-$patharray=array();
-$responsearray=array();
-function array_from_file()
-{
-
+$cardarray = array();	//this will hold all the monitored cards
+$curlarray=array();		//this is used for the parallel curl request
+$patharray=array();		//this will hold the paths for the parallel curl request
+$responsearray=array();	//this will hold the responces of each parallel curl request
+function array_from_file()	
+{	//subject to change
+	//this function reads the csv with the values for monitored cards
+	//the form of the file is cardname;cardset;prices;p1,p2,p3,p4...
 	$row = 1;
-	$price_array=array();
+	$price_array=array();	//we need an array to hold the prices
 	if (($handle = fopen("cards.csv", "r")) !== FALSE)
 	{
     	while (($data = fgetcsv($handle, 1000, ";")) !== FALSE)
     	{
-        	$num = count($data);
+        	$num = count($data);	//data holds an array with the values read
         	$name='';
         	$set='';
-       		unset($price_array);
+       		unset($price_array);	
        		$price_array=array();
         	//echo "<p> $num fields in line $row: <br /></p>\n";
         	$row++;
         	for ($c=0; $c < $num; $c++)
-        	{
+        	{	//we iterate throught the data array and assign the name and set (if c==0 and else)
         	    if ($c==0)
-        	    {	
         	    	$name=$data[$c];
-        	    	//echo $name."<br>";
-        	    }
-        	    else if($c==2)
+        	    else if($c==2)	//if c==2 then we have the prices 
         	    {	
-            		$tok=strtok($data[$c],',');
+            		$tok=strtok($data[$c],',');	//we need to tokenize them 
             		while($tok!==false)
             		{
-            			$price_array[]=$tok;
-            			//echo $tok."<br>";
+            			$price_array[]=$tok;	//and assign them to the price array
             			$tok=strtok(',');
-
             		}
             	}
             	else
-           		{	
-           			$set=$data[$c];
-           			//echo $data[$c] . "<br />\n";
-           		}
+           			$set=$data[$c];	
             } 
+            //now that we are done, we store the new entry to the cardarray
             $GLOBALS['cardarray'][$name]=array("name"=>$name,"set"=>$set,"values"=>$price_array);
-            //$GLOBALS['cardarray'][$name]=array("name"=>$name,"set"=>$set,"values"=>$price_array);
         }
     }
     fclose($handle);
-    //echo '<pre>'.print_r($cardarray,TRUE) . '</pre>';
 }
 function file_from_array()
-{
+{	//here we save the cardarray to a file
+	//we write: name;set;p1,p2,p3... where pi are the prices
 	$j=0;
 	$handle=fopen('cards.csv','w');
 	foreach($GLOBALS['cardarray'] as $key=>$value)
@@ -95,13 +88,15 @@ function add_entry($name,$set,$values)
 	$price_arr=[];
 }
 function add_value($in_str,$value)
-{
+{	//we add the price value to the previous prices
 	array_push($GLOBALS['cardarray'][$in_str]['values'],$value);
 	//echo '<pre>'.print_r($GLOBALS['cardarray'][$in_str],TRUE) . '</pre>';
 
 }
 function generate_path($in_str,$mode,$name,$set)
 {	//mode==1 is for data from array other mode is for data from db
+	//we construct the url from the card name and card set
+	//modifications must be made for special characters
 	$ret="https://www.magiccardmarket.eu/Products/Singles/";
 	if ($mode==1)
 	{	
@@ -110,10 +105,8 @@ function generate_path($in_str,$mode,$name,$set)
 		$ret=str_replace(',','%2C',$ret);
 	}
 	else
-	{
-		
 		$ret.=$set."/".replace_all($name);
-	}	
+		
 	$ret=str_replace(' ','+',$ret);
 	$ret=str_replace(',','%2C',$ret);
 	return $ret;
@@ -124,7 +117,7 @@ function replace_all($str)
 	return $str;
 }
 function improve_curl()
-{
+{	//to get the latest price for all the cards we have on offer
 	//create connection
 	$conn = mysqli_connect('localhost','root','','price_trend_getter_db');
 	//create query
@@ -146,14 +139,6 @@ function improve_curl()
 		$tmp_str=str_replace(',','%2C',$tmp_str);
 		//array to hold all paths
 		array_push($GLOBALS['patharray'],curl_init($tmp_str));
-		//individual nodes for cards
-		//$node = $dom->createElement("card");
-		//$newnode = $parnode->appendChild($node);
-		//individual node population
-		//$newnode -> setAttribute("name",$row['name']);
-		//$newnode -> setAttribute("set",$row['cset']);
-		//$newnode -> setAttribute("quantity",$row['quantity']);
-		//$newnode -> setAttribute("price",$row['price']);
 	}
 	improved_curl_execution();
 	$result=mysqli_query($conn,$sql);
@@ -176,7 +161,7 @@ function improve_curl()
 	echo $dom->saveXML();
 }
 function improved_curl_execution()
-{
+{	//parallel curl execution happens here
 	//hack for MCM to respond
 	$ua = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.A.B.C Safari/525.13';
 	//iterate through path array/individual curl init
@@ -214,7 +199,8 @@ function get_value_from_html($result)
 	return $rest;
 }
 function find_price($addr)
-{
+{	//since we have the path we can now initialize curl and get the html page with the data
+	//then we must read it to find the price we need
 	$ua = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.A.B.C Safari/525.13';
 	$ch = curl_init($addr);
 	$ptrend="Price Trend";
@@ -239,20 +225,24 @@ function find_price($addr)
 	return $rest;
 }
 function daily_update()
-{
+{	//for every card we go and obtain the latest price value
 	foreach($GLOBALS['cardarray']as $index=>$array)
 		update_single_card($index);
 }
 
 function update_single_card($index)
-{
+{	//we generate the path for the card we want
 	$path=generate_path($index,1,0,0);	//arguments are name, mode==1 for default handling and name&&set==0 (they are used for mode==2)
+	//read the html that was returned to find the price trend
 	$value=find_price($path);
+	//we replace ',' with '.' in the price
 	$value=str_replace(',','.',$value);
+	//we add the value to the previous prices
 	add_value($index,$value);
 }
 function generateXML()
-{	$dom = new DOMDocument("1.0","ISO-8859-1");
+{	//we will setup an XML to hold all the cards and the price values to send back to the server
+	$dom = new DOMDocument("1.0","ISO-8859-1");
 	$node=$dom->createElement("data");
 	$parnode = $dom->appendChild($node);
 	header("Content-type: text/xml");
@@ -316,7 +306,7 @@ function get_hint($q)
 	}
 }
 function get_cards()
-{
+{	//we setup an xml with basic information for our monitored cards, no prices.
 	$dom = new DOMDocument("1.0","ISO-8859-1");
 	$node=$dom->createElement("data");
 	$parnode = $dom->appendChild($node);
@@ -331,7 +321,8 @@ function get_cards()
 	echo $dom->saveXML();
 }
 function bought_cards_XML()
-{
+{	//we will generate the XML for the cards we have bought and sold
+	//it has information for the name,set,price,copies and action(buy,sell)
 	$dom = new DOMDocument("1.0","ISO-8859-1");
 	$node=$dom->createElement("data");
 	$parnode = $dom->appendChild($node);
@@ -354,25 +345,32 @@ function bought_cards_XML()
 	echo $dom->saveXML();
 }
 function add_bought_card($name,$set,$copies,$value,$action)
-{
+{	//we update the database with a card 
+	//$name is the card name
+	//$set is the set name
+	//$copies holds the number of cards in the transaction
+	//$value is the price of the cards (single card price)
+	//$action (buy,sell) we hold the kind of the transaction, cards purchased / cards sold
 	$conn = mysqli_connect('localhost','root','','price_trend_getter_db');
 	$sql = "INSERT INTO bought_cards(id,action,name,cset,quantity,price) VALUES(NULL,'".$action."','".$name."','".$set."',".$copies.",".$value.")";
 	mysqli_query($conn,$sql);
 	mysqli_close($conn);
 }
 function delete_a_card($index)
-{
+{	//in case we need to delete a monitored card from the cardarray we call this function with $index 
+	//the index of the card that needs to be deleted
 	unset($GLOBALS['cardarray'][$index]);
 }
-function delete_bought_card($name)
-{
+function delete_bought_card($name)//FIX?!?!?!?!?
+{	//to delete a transaction we call this function
 	$conn = mysqli_connect('localhost','root','','price_trend_getter_db');
 	$sql = "DELETE FROM bought_cards WHERE id='".$name."'";
 	mysqli_query($conn,$sql);
 	mysqli_close($conn);
 }
 function selling_cards_XML()
-{
+{	//this function returns data from the db for our offers
+	//we return the name,set,price and quantity of cards we are selling
 	$conn = mysqli_connect('localhost','root','','price_trend_getter_db');
 	$sql = "SELECT * FROM active_selling_cards";
 	$dom = new DOMDocument("1.0","ISO-8859-1");
@@ -403,30 +401,27 @@ function current_price($name,$set)
 	return $value;
 }
 function add_offer($name,$set,$copies,$price)
-{
+{	//if we want to store a new offer we call this function with
+	//$name: the name of the card
+	//$set the set of the card
+	//$copies the number of cards we have on offer
+	//$price the price of a single card
 	$conn = mysqli_connect('localhost','root','','price_trend_getter_db');
 	$sql = "INSERT INTO active_selling_cards(id,name,cset,quantity,price) VALUES(NULL,'".$name."','".$set."',".$copies.",".$price.")";
 	mysqli_query($conn,$sql);
 	mysqli_close($conn);
 }
 function remove_offer($id,$update)
-{
+{	//we remove an offer with id==$id
+	//$update is used if we want to move the card from offers to the sold cards table
 	$conn = mysqli_connect('localhost','root','','price_trend_getter_db');
 	if($update)
 	{
-		
 		$sql="SELECT * FROM active_selling_cards WHERE id=".$id;
 		$result=mysqli_query($conn, $sql);
 		$row=mysqli_fetch_assoc($result);
 		$sql="INSERT INTO bought_cards(id,action,name,cset,quantity,price)";
 		$sql.="VALUES(NULL,'sell','".$row['name']."','".$row['cset']."',".$row['quantity'].",".$row['price'].")";
-		echo $sql."<br>";
-		if(mysqli_query($conn,$sql))
-			echo "ok copy";
-		else
-		{	echo mysqli_error($conn);
-			
-		}
 	}
 	$sql = "DELETE FROM active_selling_cards WHERE id=".$id;
 	mysqli_query($conn, $sql);
@@ -434,14 +429,19 @@ function remove_offer($id,$update)
 	mysqli_close($conn);
 }
 function update($name,$set,$price,$copies,$id)
-{
+{	//when we want to update a card we call this function
+	//with $name the name of the card
+	//$set the set of the card
+	//$copies the number of cards we are selling
+	//$price the price of an individual card
 	$conn = mysqli_connect('localhost','root','','price_trend_getter_db');
 	$sql = "UPDATE active_selling_cards SET name='".$name."', cset='".$set."',quantity=".$copies.",price=".$price."WHERE id=".$id;
 	mysqli_query($conn, $sql);
 	mysqli_close($conn);
 }
 function erase_last_entries()
-{	
+{	//in case of an error or a duplicate update we can erase the latest 
+	//values for our monitored cards
 	foreach($GLOBALS['cardarray'] as $key=>$value)
 		foreach($value as $info=>$valuearr)	
 			if($info=='values')
@@ -449,7 +449,8 @@ function erase_last_entries()
 	//echo "<pre>".print_r($GLOBALS['cardarray'])."</pre>";
 	//echo "blah";
 }
-
+	//we use a single file for all server scripts 
+	//we must handle all GET requests with an if to obtain the value of the appropriate variables
 	$mode=$_GET['mode'];
 	if(isset($_GET['name']))
 		$name=$_GET['name'];
@@ -504,6 +505,7 @@ function erase_last_entries()
 	$arr=json_decode($arr);
 	var_dump($arr);
 	*/
+	//depending on the request we execute the appropriate functions
 	switch($mode)
 	{	case 1:
 			array_from_file();
